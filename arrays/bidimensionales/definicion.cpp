9f069535be107dcae784192d08a26834ca7c7bda
Cesar Osorio <cesarozorioa@hotmail.com>
2025-01-20 09:57:13 -0500

    impresion de una tabla o matriz

--- arrays/bidimensionales/definicion.cpp
+++ arrays/bidimensionales/definicion.cpp
@@ -6,4 +6,12 @@ main()
     float pesos[2][3]={ {1.2,3.4,5.6},{7.8,9.0,1.2} };//declarando e inicializando
     cout<<pesos[0][0]<<endl;//imprimiendo el primer elemento de la primera fila y primera columna
     cout<<pesos[1][1]<<endl;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            cout<<pesos[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+
+
 }
